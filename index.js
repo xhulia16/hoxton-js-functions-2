d9025783fc0d2b189c1e35e9d92ad9396ac3a67a
Xhulia Prishka <xh.prishka@gmail.com>
2022-06-21 16:35:19 +0200

    last part of excercise

--- index.js
+++ index.js
@@ -9,11 +9,19 @@ let namesContainingLetter = names.filter(function (name) {
 
 console.log(namesContainingLetter)
 
-let counter=0
-let timer=setInterval(function(){ 
-        console.log(`Hi ${namesContainingLetter[counter]}`)
-        counter++
-        if (counter === namesContainingLetter.length){
-            clearInterval(timer);}
+let counter = 0
+let timer = setInterval(function () {
+    console.log(`Hi ${namesContainingLetter[counter]}`)
+    counter++
+    if (counter === namesContainingLetter.length) {
+        clearInterval(timer);
+    }
 
-    },2000)
+}, 2000)
+
+let userId = Number(prompt("Insert your ID"))
+let incompleteUserTodos = todos
+  .filter(todo => todo.userId === userId && todo.completed === false)
+  .map(todo => todo.title)
+
+  console.log(incompleteUserTodos)
