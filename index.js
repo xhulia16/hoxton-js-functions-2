8ee09a6237b721114c185f139d19aa8fdcb81856
Xhulia Prishka <xh.prishka@gmail.com>
2022-06-21 02:52:24 +0200

    greet users

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ let letter = prompt("Insert a leter here")
 const names = users.map(user => user.name)
 console.log(names)
 
-let namesContainingLetter = names.filter(function(name){
+let namesContainingLetter = names.filter(function (name) {
     return (name.toLowerCase()).includes(letter.toLowerCase())
 })
 
 console.log(namesContainingLetter)
+
+let counter=0
+let timer=setInterval(function(){ 
+        console.log(`Hi ${namesContainingLetter[counter]}`)
+        counter++
+        if (counter === namesContainingLetter.length){
+            clearInterval(timer);}
+
+    },2000)
